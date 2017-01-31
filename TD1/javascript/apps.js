916259c5f5240cd2b357cca7f2827546ef6ec355
Léopold <trioletprojet@gmail.com>
2017-01-31 15:32:40 +0100

    --all

--- TD1/javascript/apps.js
+++ TD1/javascript/apps.js
@@ -1,14 +1,38 @@
-var mainApp = angular.module('mainApp',[]);
+'use strict';
 
-mainApp.controller('textController', function() {
+
+var mainApp = angular.module('mainApp',['ngCookies']);
+
+mainApp.controller('textController',['$cookies', function($cookies) {
 
     var self=this;
-    this.messageNote ="";
+    this.messageNote =$cookies.get('cookiesNote')||"";
     this.count=function(){
+        var caractere= 100-self.messageNote.length;
 
-    return self.messageNote.lenght;
-
-
-
+        //self.info="Note modifiée";
+        if(caractere<50 && caractere>=20)
+            self.status="alert-warning";
+        else if(caractere<20)
+            self.status="alert-danger";
+        else
+            self.status="alert-info";
+        return caractere;
     };
-});
+    this.counte=function() {
+        return 100-self.messageNote.length;
+    };
+
+    this.clear=function () {
+        self.messageNote="";
+        self.info="";
+        console.log(self.info);
+    };
+    this.save=function () {
+        self.info="Note sauvegardée";
+        self.status="alert-info";
+        $cookies.put("cookiesNote",self.messageNote);
+    };
+
+
+}]);
