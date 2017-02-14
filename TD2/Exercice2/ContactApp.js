4d99491df97646061ab056ad2561c39ad7860cae
Rodtsl <rodts.leopold@outlook.fr>
2017-02-14 09:28:20 +0100

    Exercice 2 TD2
    ng-repeat

---
+++ TD2/Exercice2/ContactApp.js
@@ -0,0 +1,4 @@
+/**
+ * Created by rodts on 14/02/2017.
+ */
+var ContactApp = angular.module("ContactApp",[]);
---
+++ TD2/Exercice2/ContactController.js
@@ -0,0 +1,27 @@
+/**
+ * Created by rodts on 14/02/2017.
+ */
+ContactApp.controller('ContactController',function() {
+
+var self=this;
+
+this.contacts= [
+    {
+       "prenom":"Mark",
+       "nom":"Zuckerberg",
+       "mail":"mark@facebook.com"
+    },
+    {
+        "prenom":"Bill",
+        "nom":"Gates",
+        "mail":"bill@microsoft.com"
+    },
+    {
+        "prenom":"Alexis",
+        "nom":"Boukhali Montagne",
+        "mail":"alexis@trash-talk.tx"
+    }
+    ];
+
+
+});
