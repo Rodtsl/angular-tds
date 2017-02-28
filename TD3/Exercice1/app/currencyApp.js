92df5029a703147e8fd711d0f0d899bbd7b957b8
Rodtsl <rodts.leopold@outlook.fr>
2017-02-28 15:22:00 +0100

    Conversion OK
    La conversion fonctionne

--- TD3/Exercice1/app/currencyApp.js
+++ TD3/Exercice1/app/currencyApp.js
@@ -1,4 +1,11 @@
 /**
  * Created by rodts on 28/02/2017.
  */
-var currencyApp = angular.module("currencyApp",[]);
+angular.module('currencyApp', [])
+    .config(['$sceDelegateProvider', function($sceDelegateProvider) {
+        // We must whitelist the JSONP endpoint that we are using to show that we trust it
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'https://free.currencyconverterapi.com/**'
+        ]);
+    }]);
--- TD3/Exercice1/app/currencyController.js
+++ TD3/Exercice1/app/currencyController.js
@@ -1,38 +1,42 @@
 /**
  * Created by rodts on 28/02/2017.
  */
-currencyApp.controller('currencyController',['$http',function($http) {
+angular.module('currencyApp').controller('currencyController',['$http',function($http) {
     var self = this;
 
     this.from;
     this.to;
     this.what=1;
     this.result;
-    this.currency;
+    this.currencies;
+    this.tmp
 
     $http.get('app/data/currencymap.json').then(function (response) {
             self.currencies = response.data;
-            self.from=self.currency.EUR;
-            self.to=self.currency.USD;
+            self.from=self.currencies.EUR;
+            self.to=self.currencies.USD;
         },
         function (response) {
             console.log("Erreur avec le statut Http : " + response.status);
         });
+    this.swap = function (){
+        self.tmp=self.to;
+        self.to=self.from;
+        self.from=self.tmp;
 
+    }
 
 
 
 
     this.getResult = function () {
-    $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q=' + from.code + '_' + to.code, {jsonpCallbackParam: 'callback'})
+    $http.jsonp('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q=' + self.from.code + '_' + self.to.code, {jsonpCallbackParam: 'callback'})
         .then(function (response) {
-            self.result = response.data[self.from.code + '_' + self.to.code].val.self.what;
+            if(angular.isDefined(response.data))
+            self.result = response.data[self.from.code + '_' + self.to.code].val*self.what;
         });
     }
 
-    this.swap = function (){
 
-
-    }
 
 }]);
